3c0f1858b41ddfe885863915b524b860f78ee65f
Ali Bajwa <abajwa@hortonworks.com>
2015-03-31 20:51:18 -0700

    change stack_dir to use cache dir to enable installing on non-ambari master hosts

--- package/scripts/params.py
+++ package/scripts/params.py
@@ -5,10 +5,10 @@ from resource_management import *
 # server configurations
 config = Script.get_config()
 
-
+stack_dir = config['configurations']['zeppelin-config']['stack.dir']
 stack_log = config['configurations']['zeppelin-config']['stack.log']
 install_dir = config['configurations']['zeppelin-config']['install.dir']
 stack_port = config['configurations']['zeppelin-config']['stack.port']
 mvn_dir = config['configurations']['zeppelin-config']['mvn.dir']
-stack_dir = config['configurations']['zeppelin-config']['stack.dir']
+
 
